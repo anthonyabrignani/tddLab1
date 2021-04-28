957afdefb6e94fadf5480c502cc79eb651aaff39
anthony abrignani <anthony abrignani>
2021-04-27 23:23:32 -0400

    a couple hours reading, it all works now

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-function translate (string) {
-    string = string.toLowerCase();
+function translate(string) {
+  string = string.toLowerCase();
 
-    let vowels = ["a", "e", "i", "o", "u"]
+  let vowels = ["a", "e", "i", "o", "u"];
 
-    if (vowels.indexOf(string[0]) > -1) { 
-        return string + "way";
-    } else {
-        
-    }
-    
+  if (vowels.indexOf(string[0]) > -1) {
+    return string + "way";
+  } else {
+    let firstMatch = string.match(/[aeiou]/g) || 0;
+    let firstVowel = string.indexOf(firstMatch[0]);
+    return string.substring(firstVowel) + string.substring(0, firstVowel) + "ay";
+  }
 }
- 
+
 module.exports.translate = translate;
--- test.js
+++ test.js
@@ -2,34 +2,48 @@ const { TestScheduler } = require("@jest/core");
 const { translate } = require("./index");
 
 describe("translator", () => {
+  test("lowercase starts with vowel", () => {
+    let result = translate("omni");
+    expect(result).toEqual("omniway");
+  });
 
-    test("lowercase starts with vowel", () => {
-        let result = translate("omni")
-        expect(result).toEqual("omniway")
-    })
+  test("lowercase starts with 2 vowels", () => {
+    let result = translate("eerie");
+    expect(result).toEqual("eerieway");
+  });
 
-    test("lowercase starts with 2 vowels", () => {
-        let result = translate("eerie")
-        expect(result).toEqual("eerieway")
-    })
+  test("lowercase starts with capital vowel", () => {
+    let result = translate("Infantry");
+    expect(result).toEqual("infantryway");
+  });
 
-    test("lowercase starts with capital vowel", () => {
-        let result = translate("Infantry")
-        expect(result).toEqual("infantryway")
-    })
+  test("lowercase starts with capital vowel", () => {
+    let result = translate("Anthony");
+    expect(result).toEqual("anthonyway");
+  });
 
-    test("lowercase starts with capital vowel", () => {
-        let result = translate("Anthony")
-        expect(result).toEqual("anthonyway")
-    })
+  test("uppercase conversion, also starts with vowel", () => {
+    let result = translate("URALS");
+    expect(result).toEqual("uralsway");
+  });
 
-    test("uppercase conversion, aloso starts with vowel", () => {
-        let result = translate("URALS")
-        expect(result).toEqual("uralsway")
-    })
+  test("starts with 1 consonant", () => {
+    let result = translate("gamble");
+    expect(result).toEqual("amblegay");
+  });
 
-    test("starts with 1 consonant", () => {
-        let result = translate("gamble")
-        expect(result).toEqual("amblegay")
-    })
-})
+  test("starts with 1 consonant", () => {
+    let result = translate("gamble");
+    expect(result).toEqual("amblegay");
+  });
+
+  test("starts with 2 consonants", () => {
+    let result = translate("create");
+    expect(result).toEqual("eatecray");
+  });
+
+  test("starts with 3 consonants", () => {
+    let result = translate("shrinking");
+    expect(result).toEqual("inkingshray");
+  });
+});
